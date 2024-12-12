75294399b8816fb9487de27a2f6d316ad9a77120
Miroslav Viktorin <miroslav.viktori@seznam.cz>
2024-12-12 13:14:52 +0100

    makame jedeme

--- js/script.js
+++ js/script.js
@@ -5,5 +5,15 @@
 // -----------------
 
 $(function () {
-   
+  var btm_frame = $(".image__banner.banner--bottom-frame");
+  var top_frame = $(".image__banner.banner--top-frame");
+  var btm_about = $(".about--bottom");
+  var top_about = $(".about--top");
+
+  btm_frame.on("click", function () {
+    btm_about.fadeToggle(300, function () {});
+  });
+  top_frame.on("click", function () {
+    top_about.fadeToggle(300, function () {});
+  });
 });
