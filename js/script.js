4bff9d562ef176733d2bd8b810170dd9ae3f2d28
Miroslav Viktorin <miroslav.viktori@seznam.cz>
2024-12-13 17:32:02 +0100

    makame

--- js/script.js
+++ js/script.js
@@ -5,10 +5,35 @@
 // -----------------
 
 $(function () {
-  var btm_frame = $(".image__banner.banner--bottom-frame");
-  var top_frame = $(".image__banner.banner--top-frame");
-  var btm_about = $(".about--bottom");
-  var top_about = $(".about--top");
+  const toTattoo = $("#toTatoo");
+  const toBeauty = $("#toBeauty");
+  const tattoo = $("#tattoo");
+  const beauty = $("#beauty");
+  const offset = 70;
+
+  const btm_frame = $(".image__banner.banner--bottom-frame");
+  const top_frame = $(".image__banner.banner--top-frame");
+  const btm_about = $(".about--bottom");
+  const top_about = $(".about--top");
+
+  toTattoo.on("click", function (event) {
+    event.preventDefault();
+    $("html, body").animate(
+      {
+        scrollTop: tattoo.offset().top - offset,
+      },
+      1
+    );
+  });
+  toBeauty.on("click", function (event) {
+    event.preventDefault();
+    $("html, body").animate(
+      {
+        scrollTop: beauty.offset().top - offset,
+      },
+      1
+    );
+  });
 
   btm_frame.on("click", function () {
     btm_about.fadeToggle(300, function () {});
