9a9bc2da81339c5ad657045f17d5a3907ab57944
Miroslav Viktorin <miroslav.viktori@seznam.cz>
2024-12-21 19:03:48 +0100

    big update

--- js/gallery.js
+++ js/gallery.js
@@ -4,4 +4,21 @@
 // $(function () {});
 // -----------------
 
-$(function () {});
+$(function () {
+  const switcher = $(".gallery__list .switcher");
+  const tattooList = $("#tattoo__list");
+  const beautyList = $("#beauty__list");
+
+  switcher.on("click", function () {
+    switcher.removeClass("active");
+    $(this).addClass("active");
+
+    if ($(this).text() === "beauty") {
+      beautyList.removeClass("black__hole");
+      tattooList.addClass("black__hole");
+    } else {
+      tattooList.removeClass("black__hole");
+      beautyList.addClass("black__hole");
+    }
+  });
+});
